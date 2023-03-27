87e3ee8b2ce9e11c0c77b993c43e81ec20ea8440
vijay6713 <129036987+vijay6713@users.noreply.github.com>
2023-03-27 12:06:14 +0530

    Add files via upload

---
+++ ProjectPractice2.sql
@@ -0,0 +1,8 @@
+Use Vijay;
+SELECT * FROM vijay.financialreport;
+Select * from financialreport;
+Select AVG(Sales) from financialreport order by Product ASC;
+Select distinct ID from financialreport;
+Select distinct country from financialreport;
+Select  distinct Segment,country,Product,Discount_Band,Units_sold,Manufacturing_Price,Sale_Price,Gross_Sales,Discounts,Sales,COGS,Profit,Date,Month_number,Month_Name,Year from
+financialreport;
